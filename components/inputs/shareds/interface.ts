616677220b09a3dd8297cbddbee17b61bf9b441b
Eric <minh.hvt@cyberlogitec.com>
2025-03-04 04:38:33 -0300

    Fix: yarn build and update config eslint

--- components/inputs/shareds/interface.ts
+++ components/inputs/shareds/interface.ts
@@ -1,4 +1,4 @@
-import { ChangeEventHandler, FocusEventHandler, HTMLInputTypeAttribute } from "react";
+import { ChangeEventHandler, FocusEventHandler } from "react";
 
 export interface IInputCommonProps {
   label?: string;
